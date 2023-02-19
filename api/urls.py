4322e1110d4ad3551a0d0463b4098cc3d95419ba
ShoeRiderr <kamilsocha20@gmail.com>
2023-02-19 12:14:39 +0100

    Add rest authorization with token

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import UserRecordView
 
+app_name = 'api'
 urlpatterns = [
     path('drivers-routes/', views.driverRoutes),
-    path('drivers-routes/<str:id>/', views.driverRoute)
+    path('drivers-routes/<str:id>/', views.driverRoute),
+    path('users/', UserRecordView.as_view(), name='users'),
 ]
--- inzynierka_tracking_app_backend/urls.py
+++ inzynierka_tracking_app_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
